65949ffa85023097fb82e3179c2ade4f7a9207c0
Turowski <turowski@med.umich.edu>
2024-08-21 10:51:23 -0400

    adjusted code.py to add documentation & improve readability

--- code.py
+++ code.py
@@ -1,16 +1,36 @@
-def function1(a):
-    b = dict()
-    for c in a:
-        if c not in b:
-            b[c] = 1
+def calculate_nucleotide_frequencies(sequence):
+    """
+    Calculate the frequency of each nucleotide in the given DNA sequence.
+
+    Args:
+    sequence (str): A string representing the DNA sequence.
+
+    Returns:
+    dict: A dictionary where keys are nucleotides and values are their respective frequencies.
+    """
+    frequency_dict = dict()
+    for nucleotide in sequence:
+        if nucleotide not in frequency_dict:
+            frequency_dict[nucleotide] = 1
         else:
-            b[c] += 1
-    return b
+            frequency_dict[nucleotide] += 1
+    return frequency_dict
 
-def function2(a):
-    print('freqs')
-    total = float(sum([a[b] for b in a.keys()]))
-    for b in a.keys():
-        print(b + ':' + str(a[b]/total))
+def print_nucleotide_frequencies(frequency_dict):
+    """
+    Print the relative frequency of each nucleotide in the frequency dictionary.
 
-function2(function1('ATCTGACGCGCGCCGC'))
+    Args:
+    frequency_dict (dict): A dictionary containing nucleotide frequencies.
+    """
+    print('Nucleotide Frequencies:')
+    total_count = float(sum(frequency_dict.values()))
+    
+    for nucleotide, count in frequency_dict.items():
+        relative_frequency = count / total_count
+        print(f'{nucleotide}: {relative_frequency:.4f}')
+
+# Example usage
+sequence = 'ATCTGACGCGCGCCGC'
+frequencies = calculate_nucleotide_frequencies(sequence)
+print_nucleotide_frequencies(frequencies)
